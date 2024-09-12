716b92c71285a593f60aebcd11ca60749a021155
Sarah Engel <justsweetluna@gmail.com>
2024-09-12 17:21:52 +0200

    Update addon to new Gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,19 +1,16 @@
-version = "0.1.6"
-
-plugins {
-    id("java-library")
-}
+import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtension.ReferenceType
 
 dependencies {
+    labyProcessor()
     api(project(":api"))
-    maven(mavenCentral(), "org.apache.httpcomponents:httpclient:4.5.14")
+    addonMavenDependency("org.apache.httpcomponents:httpclient:4.5.14")
+
+    // An example of how to add an external dependency that is used by the addon.
+    // addonMavenDependency("org.jeasy:easy-random:5.0.0")
 }
 
-labyModProcessor {
-    referenceType = net.labymod.gradle.core.processor.ReferenceType.DEFAULT
+labyModAnnotationProcessor {
+    referenceType = ReferenceType.DEFAULT
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
+
